648dcbd7f5d473ae1f382b9937298ab655e08e4f
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-10-24 01:29:44 +0900

    refine structure

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/Mistobaan/sqs"
@@ -11,7 +12,7 @@ import (
 	"os/exec"
 )
 
-type notify struct {
+type SQSNotify struct {
 	auth   aws.Auth
 	region aws.Region
 	name   string
@@ -19,17 +20,17 @@ type notify struct {
 	queue *sqs.Queue
 }
 
-func NewNotify(auth aws.Auth, region aws.Region, name string) *notify {
-	return &notify{auth, region, name, nil}
+func New(auth aws.Auth, region aws.Region, name string) *SQSNotify {
+	return &SQSNotify{auth, region, name, nil}
 }
 
-func (n *notify) Open() (err error) {
+func (n *SQSNotify) Open() (err error) {
 	awsSQS := sqs.New(n.auth, n.region)
 	n.queue, err = awsSQS.GetQueue(n.name)
 	return
 }
 
-func (n *notify) Listen(handler func(*sqs.Message, func() error) error) (err error) {
+func (n *SQSNotify) Listen(handler func(*sqs.Message, func() error) error) (err error) {
 	for {
 		resp, err := n.queue.ReceiveMessage(10)
 		if err != nil {
@@ -46,7 +47,7 @@ func (n *notify) Listen(handler func(*sqs.Message, func() error) error) (err err
 	}
 }
 
-func (n *notify) deleter(m *sqs.Message) func() error {
+func (n *SQSNotify) deleter(m *sqs.Message) func() error {
 	deleted := false
 	return func() (err error) {
 		if deleted {
@@ -55,61 +56,6 @@ func (n *notify) deleter(m *sqs.Message) func() error {
 		deleted = true
 		_, err = n.queue.DeleteMessage(m)
 		return
-	}
-}
-
-func openQueue(auth aws.Auth, region aws.Region, name string) (queue *sqs.Queue, err error) {
-	awsSQS := sqs.New(auth, aws.APNortheast)
-	queue, err = awsSQS.GetQueue(name)
-	return
-}
-
-func dispatchMessages(queue *sqs.Queue, messages []sqs.Message, bodyHandler func(string) error) []error {
-	// Prepare error list.
-	errorList := make([]error, len(messages))
-	errorCount := 0
-
-	// Prepare to delete received messages.
-	deleteList := make([]sqs.Message, len(messages))
-	deleteCount := 0
-	defer func() {
-		if deleteCount > 0 {
-			go func() {
-				resp, err := queue.DeleteMessageBatch(deleteList[0:deleteCount])
-				if err != nil {
-					log.Println("failed to delele messages", err, resp)
-					recover()
-				}
-			}()
-		}
-	}()
-
-	// Dispatch all messages.
-	for _, m := range messages {
-		err2 := bodyHandler(m.Body)
-		if err2 != nil {
-			errorList[errorCount] = err2
-			errorCount += 1
-			continue
-		}
-		deleteList[deleteCount] = m
-		deleteCount += 1
-	}
-
-	return errorList[0:errorCount]
-}
-
-func listenQueue(queue *sqs.Queue, bodyHandler func(string) error) (err error) {
-	for {
-		resp, err := queue.ReceiveMessage(10)
-		if err != nil {
-			return err
-		}
-
-		errs := dispatchMessages(queue, resp.Messages, bodyHandler)
-		if errs != nil && len(errs) > 0 {
-			return errs[0]
-		}
 	}
 }
 
@@ -152,7 +98,7 @@ func runCmd(cmd *exec.Cmd, msgbody string) (err error) {
 	return cmd.Wait()
 }
 
-func args2notify() *notify {
+func args2notify() (*SQSNotify, error) {
 	var regionName string
 	flag.StringVar(&regionName, "region", "us-east-1",
 		"AWS Region for queue")
@@ -166,26 +112,25 @@ func args2notify() *notify {
 	queueName := args[0]
 
 	// Determine a region.
-	region, flag := aws.Regions[regionName]
-	if !flag {
-		log.Fatalln("sqs-notify:", "unknown region:", regionName)
+	region, ok := aws.Regions[regionName]
+	if !ok {
+		return nil, errors.New("unknown region:" + regionName)
 	}
 
 	// Retrieve an AWS auth.
 	auth, err := aws.EnvAuth()
 	if err != nil {
-		log.Fatalln("sqs-notify:", err)
+		return nil, err
 	}
 
-	return NewNotify(auth, region, queueName)
+	return New(auth, region, queueName), nil
 }
 
-func run(n *notify) (err error) {
+func run(n *SQSNotify) (err error) {
 	// Open a queue.
 	err = n.Open()
 	if err != nil {
 		return
-		log.Fatalln("sqs-notify:", err)
 	}
 
 	// Listen the queue.
@@ -197,8 +142,11 @@ func run(n *notify) (err error) {
 }
 
 func main() {
-	n := args2notify()
-	err := run(n)
+	n, err := args2notify()
+	if err != nil {
+		log.Fatalln("sqs-notify:", err)
+	}
+	err = run(n)
 	if err != nil {
 		log.Fatalln("sqs-notify:", err)
 	}
