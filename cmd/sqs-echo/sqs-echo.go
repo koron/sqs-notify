950691dc771e757dbd5272aa980413cf65b0ba47
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-06-01 11:53:41 +0900

    sleep option for sqs-echo

--- cmd/sqs-echo/sqs-echo.go
+++ cmd/sqs-echo/sqs-echo.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"flag"
 	"io/ioutil"
 	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	sleep := flag.Int("sleep", 0, "sleep seconds after output (default: 0)")
+	flag.Parse()
 	b, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
 	}
 	log.Printf("(%d) %q", len(b), b)
+	time.Sleep(time.Duration(*sleep) * time.Second)
 }
