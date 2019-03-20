7e73db717dfbd05a67af3e672c9455b63724bfe1
MURAOKA Taro <koron.kaoriya@gmail.com>
2019-03-20 12:30:15 +0900

    redis URL can't include DBnum

--- sqsnotify2/cache_redis.go
+++ sqsnotify2/cache_redis.go
@@ -31,7 +31,7 @@ func newRedisCache(ctx context.Context, u *url.URL) (*redisCache, error) {
 		opt.Password, _ = u.User.Password()
 	}
 	if u.Path != "" {
-		opt.DB, err = strconv.Atoi(u.Path)
+		opt.DB, err = strconv.Atoi(u.Path[1:])
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse path as DB num: %s", err)
 		}
